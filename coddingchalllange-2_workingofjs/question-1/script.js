221e6a743df83654878edd34bd7c46c6de16b061
Sujit-Panigrahi5 <panigrahisujit588@gmail.com>
2023-09-03 23:56:15 +0530

    add some code

---
+++ coddingchalllange-2_workingofjs/question-1/script.js
@@ -0,0 +1,8 @@
+let a=4;
+let b=5;
+let c=addNumbers()
+console.log(c)
+function addNumbers(){
+    let c=a+b;
+    return c;
+}
---
+++ coddingchalllange-2_workingofjs/question-2/script.js
@@ -0,0 +1,8 @@
+let a=4;
+let b=5;
+let c=multiplyNumbers()
+console.log(c)
+function multiplyNumbers(){
+    let c=a*b;
+    return c;
+}
