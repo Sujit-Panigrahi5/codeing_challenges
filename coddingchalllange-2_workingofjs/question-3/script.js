8f5f17c987b1d64ee420ad59b0b615aa75f70489
Sujit-Panigrahi5 <panigrahisujit588@gmail.com>
2023-09-04 10:19:53 +0530

    add all code

---
+++ coddingchalllange-2_workingofjs/question-3/script.js
@@ -0,0 +1,12 @@
+
+
+let a=4;
+let b=5;
+
+function sum(a,b){
+    var c;
+    console.log(c);
+    c=a+b;
+    return c;
+}
+let c=sum(a,b)
---
+++ coddingchalllange-2_workingofjs/question-4/script.js
@@ -0,0 +1,25 @@
+
+// a=5
+// b=4
+// c=3
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+{
+    //     a=5
+    // b=4
+    // c=3
+    // console.log(a)
+    // console.log(b)
+    // // console.log(c)
+    // var a;
+    // let b;
+    // const c
+    // a=5
+    // b=4
+    // c=3
+    // console.log(a)
+    // console.log(b)
+    // console.log(c)
+}
